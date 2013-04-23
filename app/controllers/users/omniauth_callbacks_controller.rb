b9e25083f071b17e4120d05eff3045962de90b91
toshihiro SEKIOKA <tsekioka@miwa-lock.co.jp>
2013-04-23 19:51:16 +0900

    apiを追加

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,9 +1,26 @@
-class Users::OmniauthCallbacksController < ApplicationController
+﻿class Users::OmniauthCallbacksController < ApplicationController
+  CLIENT_SCHEME = "com.dennou.pm"
+
   def twitter
     auth = request.env['omniauth.auth']
     logger.debug auth.inspect
     @user = User.find_or_create_by_twitter(auth)
+
+    #アプリからのアクセスなら
+    if request.env['HTTP_USER_AGENT'].include?('PmClient') then
+      return redirect_to "#{CLIENT_SCHEME}://local/auth?token=#{@user.authentication_token}"
+    end
+
     sign_in @user
-    redirect_to '/'
+    redirect_to root_path
+  end
+  
+  def failure
+    flash[:notice] = "ログインに失敗しました"
+    #アプリからのアクセスなら
+    if request.env['HTTP_USER_AGENT'].include?('PmClient') then
+      return redirect_to "#{CLIENT_SCHEME}://local/error"
+    end
+    redirect_to root_path
   end
 end
