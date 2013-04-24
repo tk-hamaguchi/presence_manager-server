54bcc4d5e866ff88e96c3cbca8d0a3d9373a2875
toshihiro SEKIOKA <esper776@hotmail.com>
2013-04-24 22:29:01 +0900

    テスト用データ作成用 seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require "factory_girl"
+FactoryGirl.find_definitions
+
+if Rails.env == "development"
+  User.delete_all
+  Venue.delete_all
+  Seat.delete_all
+  NfcTag.delete_all
+  
+  venue = FactoryGirl.create :venue_with_seats, seats_count:10
+  user = FactoryGirl.create :user
+  user.venues << venue
+end
+
+
