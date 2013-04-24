6bba858a46f39719cc5b734ab3feb032ff8db766
toshihiro SEKIOKA <esper776@hotmail.com>
2013-04-25 07:37:29 +0900

    署名をnfc_tagにした。seminara_controllerに署名のチェックを追加

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,14 +1,6 @@
-class Seat < ActiveRecord::Base
+﻿class Seat < ActiveRecord::Base
   include OpenSSL
 
   belongs_to :venue
   has_one :nfc_tag
-
-  def sign
-    sign = OpenSSL::HMAC.digest(
-             Constants.nfc.sign_logic,
-             Constants.nfc.secret,
-             ("%08X" % self.id) )
-    Base64.strict_encode64 sign
-  end
 end
