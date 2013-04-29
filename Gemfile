d61e4cf0a7ccce2ed01db15414da58c105a41ca2
Takahiro HAMAGUCHI <tk.hamaguchi@gmail.com>
2013-04-30 01:57:31 +0900

    assets compile with DATABASE_URL

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'http://rubygems.org'
 
 ruby "2.0.0"
 
-gem 'rails', '4.0.0.beta1'
+gem 'rails', '4.0.0.rc1'
 
 
 gem 'devise', git:'git://github.com/plataformatec/devise.git', branch:'rails4'
--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,5 @@ module PresenceManager
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
 
-    config.assets.initialize_on_precompile = false
-
   end
 end
